0229995c1607b1ddbc787c0d3a71bc2cf7cd76d2
a710323 <a71032340929@gmail.com>
2021-04-24 12:31:59 +0800

    add views

---
+++ Stockify/Views/NewsInfoCell.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+class NewsInfoCell : UITableViewCell {
+    @IBOutlet weak var newsTitleLabel: UILabel?
+    
+    @IBOutlet weak var descriptionNewsLabel: UILabel?
+    
+    
+    
+    
+}
+
